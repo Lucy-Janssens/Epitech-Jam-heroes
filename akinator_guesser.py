042ce3c9c5852f05d900df0bb7e3e56844776719
Lucy Janssens <guillaume.janssens@epitech.eu>
2023-02-12 18:08:07 +0100

    [+] adding completed game, with images, fix to come on question after end.

--- akinator_guesser.py
+++ akinator_guesser.py
@@ -6,12 +6,13 @@ from PySide6 import QtCore, QtWidgets, QtGui
 aki = None
 first_question = None
 
-heroes = ["Superman", "Batman", "Spiderman", "CatWoman"]
+heroes = ["Superman", "Batman", "Spiderman", "Catwoman", "Ironman", "Black Panther", "Deadpool", "Captain America",
+          "Antman", "Captain Marvel", "Wolverine", "Beast Boy"]
 
 # TODO: add super villain qui stoppe le jeu avec une certaine probabilité
-# TODO: add a retry button
-# TODO: add a button to show the picture of the hero
+# TODO: show the hero image (and fetch it through api)
 # TODO: add a leaderboard
+# TODO: modify to grid layout
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
@@ -20,11 +21,14 @@ class MyWidget(QtWidgets.QWidget):
 
         self.game_title = QtWidgets.QLabel("Make It Remember!",
                                      alignment=QtCore.Qt.AlignCenter)
-        self.hero_name = QtWidgets.QLabel("Make me guess : " + rd.choice(heroes),
+        self.hero = rd.choice(heroes)
+        self.hero_name = QtWidgets.QLabel("Make me guess : " + self.hero,
                                           alignment=QtCore.Qt.AlignCenter)
         self.display_counter = QtWidgets.QLabel("Number of questions answered: " + str(self.counter),
                                                 alignment=QtCore.Qt.AlignCenter)
         self.current_question = QtWidgets.QLabel(first_question, alignment=QtCore.Qt.AlignCenter)
+
+        self.img = self.hero + ".png"
 
         self.btn_yes = QtWidgets.QPushButton("Yes")
         self.btn_no = QtWidgets.QPushButton("No")
@@ -33,18 +37,36 @@ class MyWidget(QtWidgets.QWidget):
         self.btn_prob_no = QtWidgets.QPushButton("Probably Not")
         self.btn_finish = QtWidgets.QPushButton("Show me the prediction")
 
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.game_title)
-        self.layout.addWidget(self.hero_name)
-        self.layout.addWidget(self.display_counter)
-        self.layout.addWidget(self.current_question)
+        self.layout = QtWidgets.QHBoxLayout(self)
 
-        self.layout.addWidget(self.btn_yes)
-        self.layout.addWidget(self.btn_no)
-        self.layout.addWidget(self.btn_dk)
-        self.layout.addWidget(self.btn_prob)
-        self.layout.addWidget(self.btn_prob_no)
-        self.layout.addWidget(self.btn_finish)
+        self.left_layout = QtWidgets.QVBoxLayout(self)
+        self.pixmap = QtGui.QPixmap("./images/" + self.img)
+        self.pixmap = self.pixmap.scaled(400, 600, QtCore.Qt.KeepAspectRatio, QtCore.Qt.FastTransformation)
+        self.image = QtWidgets.QLabel()
+        self.image.setPixmap(self.pixmap)
+        self.image.show()
+        self.left_layout.addWidget(self.image)
+
+        self.middle_layout = QtWidgets.QVBoxLayout(self)
+        self.middle_layout.addWidget(self.game_title)
+        self.middle_layout.addWidget(self.hero_name)
+        self.middle_layout.addWidget(self.display_counter)
+        self.middle_layout.addWidget(self.current_question)
+
+        self.middle_layout.addWidget(self.btn_yes)
+        self.middle_layout.addWidget(self.btn_no)
+        self.middle_layout.addWidget(self.btn_dk)
+        self.middle_layout.addWidget(self.btn_prob)
+        self.middle_layout.addWidget(self.btn_prob_no)
+        self.middle_layout.addWidget(self.btn_finish)
+
+        self.right_layout = QtWidgets.QVBoxLayout(self)
+        self.leaderboard = QtWidgets.QLabel("Leaderboard", alignment=QtCore.Qt.AlignCenter)
+        self.right_layout.addWidget(self.leaderboard)
+
+        self.layout.addLayout(self.left_layout)
+        self.layout.addLayout(self.middle_layout)
+        self.layout.addLayout(self.right_layout)
 
         self.btn_yes.clicked.connect(self.on_click_yes)
         self.btn_no.clicked.connect(self.on_click_no)
@@ -55,6 +77,9 @@ class MyWidget(QtWidgets.QWidget):
 
         self.setWindowTitle("Make It Remember")
 
+    def create_leaderboard(self):
+        pass
+
     def update_counter(self):
         self.counter += 1
         self.display_counter.setText("Number of questions answered: " + str(self.counter))
@@ -64,10 +89,19 @@ class MyWidget(QtWidgets.QWidget):
         aki.win()
         to_show = "The genius has guessed the following hero: " + aki.first_guess['name']
 
+        if self.hero in aki.first_guess['name']:
+            to_show += "\nYou win!"
+
         self.counter = -1
         self.update_counter()
-        self.hero_name.setText("Make me guess: " + rd.choice(heroes))
-
+        self.hero = rd.choice(heroes)
+        self.img = self.hero + ".png"
+        self.hero_name.setText("Make me guess: " + self.hero)
+        self.left_layout = QtWidgets.QVBoxLayout(self)
+        self.pixmap = QtGui.QPixmap("./images/" + self.img)
+        self.pixmap = self.pixmap.scaled(400, 600, QtCore.Qt.KeepAspectRatio, QtCore.Qt.FastTransformation)
+        self.image.setPixmap(self.pixmap)
+        self.image.show()
         self.current_question.setText(to_show)
 
     @QtCore.Slot()
@@ -109,7 +143,7 @@ if __name__ == '__main__':
     first_question = aki.start_game()
 
     widget = MyWidget()
-    widget.resize(800, 600)
+    widget.resize(1200, 600)
     widget.show()
 
     sys.exit(app.exec())
